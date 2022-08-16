6d819f8e3837c3e2ca0739fe894f7d813d0fded5
Marilou Pichol <marilou.pichol@laposte.net>
2022-08-16 15:42:58 +0200

    rails-task-manager

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,12 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   primary_abstract_class
-  def change
-    create_table :tasks do |t|
-      t.string :name
-      t.text :details
-      t.booelan :completed, default: 0
-
-      t.timestamps
-    end
-  end
 end
